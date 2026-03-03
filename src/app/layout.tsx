00a6cd1d0a4f0cb7d5d8584d6a38df08e96a2549
Tracy Chung <chuiyingchungccy@gmail.com>
2026-03-02 21:54:20 -0800

    Fix background image path and add suppressHydrationWarning for html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,13 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Jersey+20&family=Jersey+25&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="antialiased">
+      <body
+        className="antialiased"
+        style={{
+          backgroundImage: `url('${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/background.png')`,
+        }}
+      >
         {children}
       </body>
     </html>
